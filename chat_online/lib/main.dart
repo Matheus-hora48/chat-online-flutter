ba0c5a1127186605e9edde38163edcd89a2a6c0b
Matheus Hora <matheus11hora@gmail.com>
2022-09-03 19:19:45 -0300

    Novos

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:chat_online/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const Home());
@@ -18,12 +19,11 @@ class Home extends StatelessWidget {
       title: 'Chat Flutter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
-        iconTheme: const IconThemeData(
-          color: Colors.lightGreen,
-        )
-      ),
-      home: Container(),
+          primarySwatch: Colors.lightGreen,
+          iconTheme: const IconThemeData(
+            color: Colors.lightGreen,
+          )),
+      home: ChatScreen(),
     );
   }
 }
